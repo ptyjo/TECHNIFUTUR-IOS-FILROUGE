1fada997a88cca639b793fe402ded8de73f715b8
jorthi <joris.thiery@smile.fr>
2021-05-31 12:30:04 +0200

    feat: Add layout

--- FilRouge/FilRouge/ViewController.swift
+++ FilRouge/FilRouge/ViewController.swift
@@ -9,9 +9,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //TEST
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+
+        let a = 1
     }
 
 
